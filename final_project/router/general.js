7838d5e01c4bb241f858667a4182bed4409b714c
Sergio Peña <sergiop79@gmail.com>
2023-12-03 10:05:16 +0100

    Task 6 done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,8 +5,19 @@ let users = require('./auth_users.js').users;
 const public_users = express.Router();
 
 public_users.post('/register', (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: 'Yet to be implemented' });
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ message: 'Username and password are required' });
+  }
+  if (users[username]) {
+    return res.status(409).json({ message: 'User already exists' });
+  }
+
+  users[username] = password;
+  return res.status(201).json({ message: 'User created successfully' });
 });
 
 // Get the book list available in the shop
