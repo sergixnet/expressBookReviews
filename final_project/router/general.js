d4ac965560c9cd00f4b4ef4c810ff7ccaab64b9e
Sergio Peña <sergiop79@gmail.com>
2023-12-03 11:40:00 +0100

    Remove commented code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,8 +23,6 @@ public_users.post('/register', (req, res) => {
 
 // Get the book list available in the shop
 public_users.get('/', async function (req, res) {
-  // return res.json(books);
-
   try {
     const data = await booksService.getAll();
     return res.json(data);
@@ -36,12 +34,6 @@ public_users.get('/', async function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
-  // const isbns = Object.keys(books);
-  // if (isbns.includes(isbn)) {
-  //   return res.json(books[isbn]);
-  // } else {
-  //   return res.status(404).json({ message: 'Book not found' });
-  // }
 
   booksService
     .getByISBN(isbn)
@@ -57,21 +49,6 @@ public_users.get('/isbn/:isbn', function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
   const author = req.params.author;
-  // const isbns = Object.keys(books);
-  // let result = [];
-  // for (let isbn of isbns) {
-  //   if (books[isbn].author === author) {
-  //     result.push(books[isbn]);
-  //   }
-  // }
-  // if (result.length > 0) {
-  //   return res.json(result);
-  // } else {
-  //   return res
-  //     .status(404)
-  //     .json({ message: 'Books with this author not found' });
-  // }
-
   booksService
     .getBooksByAuthor(author)
     .then((data) => {
@@ -86,18 +63,6 @@ public_users.get('/author/:author', function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
   const title = req.params.title;
-  // const isbns = Object.keys(books);
-  // let result = [];
-  // for (let isbn of isbns) {
-  //   if (books[isbn].title === title) {
-  //     result.push(books[isbn]);
-  //   }
-  // }
-  // if (result.length > 0) {
-  //   return res.json(result);
-  // } else {
-  //   return res.status(404).json({ message: 'Books with this title not found' });
-  // }
 
   booksService
     .getBookByTitle(title)
