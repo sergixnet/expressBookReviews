1f351ccc781108591bcf1b95deba312c4d3db586
Sergio Peña <sergiop79@gmail.com>
2023-12-03 10:00:31 +0100

    Task 5 done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -63,8 +63,13 @@ public_users.get('/title/:title', function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: 'Yet to be implemented' });
+  const isbn = req.params.isbn;
+  const isbns = Object.keys(books);
+  if (isbns.includes(isbn)) {
+    return res.json(books[isbn].reviews);
+  } else {
+    return res.status(404).json({ message: 'Book not found' });
+  }
 });
 
 module.exports.general = public_users;
