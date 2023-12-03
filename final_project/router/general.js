540477496bc64d74a2c9679b4aaeda8ea24c1ff4
Sergio Peña <sergiop79@gmail.com>
2023-12-03 09:55:00 +0100

    Task 3 done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,8 +27,21 @@ public_users.get('/isbn/:isbn', function (req, res) {
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: 'Yet to be implemented' });
+  const author = req.params.author;
+  const isbns = Object.keys(books);
+  let result = [];
+  for (let isbn of isbns) {
+    if (books[isbn].author === author) {
+      result.push(books[isbn]);
+    }
+  }
+  if (result.length > 0) {
+    return res.json(result);
+  } else {
+    return res
+      .status(404)
+      .json({ message: 'Books with this author not found' });
+  }
 });
 
 // Get all books based on title
